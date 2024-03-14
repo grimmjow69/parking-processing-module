d71cf174ab33e9cbab517c5a033f51d281c8977e
fucek2 <fucek2@stud.uniza.sk>
2024-03-14 19:01:38 +0100

    cron job for cleaning db

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ const app = express();
 
 app.use(express.json());
 
+const db = require("./db-connection");
+
 const cron = require("node-cron");
 const authRoutes = require("./routes/auth-routes");
 const notificationRoutes = require("./routes/notification-routes");
 const parkingSpotRoutes = require("./routes/parking-spot-routes");
 const userRoutes = require("./routes/user-routes");
+
 const ExternalApiService = require("./services/external-api.service");
+const DataRetentionService = require("./services/data-retention-service");
 
 app.use("/auth", authRoutes);
 app.use("/notification", notificationRoutes);
@@ -19,16 +23,29 @@ app.use("/parking", parkingSpotRoutes);
 app.use("/user", userRoutes);
 
 const externalApiService = new ExternalApiService();
+const dataRetentionService = new DataRetentionService(db);
 
 app.listen(process.env.PORT, function () {
   console.log(`Server running on port ${process.env.PORT}`);
 });
 
-// cron.schedule("*/1 * * * *", async () => {
-//   try {
-//     await externalApiService.updateParkingLotsWithNewData();
-//     console.log("Parking spots updated successfully.");
-//   } catch (error) {
-//     console.error("Error while updating parking spots:", error.message);
-//   }
-// });
+cron.schedule("59 23 * * 6", async () => {
+  try {
+    await dataRetentionService.cleanUpParkingHistory();
+    console.log("History of parking cleaned up successfully");
+  } catch (error) {
+    console.error(
+      "Error while parking cleaning up parking history:",
+      error.message
+    );
+  }
+});
+
+cron.schedule("*/15 * * * *", async () => {
+  try {
+    await externalApiService.updateParkingLotsWithNewData();
+    console.log("Parking spots updated successfully.");
+  } catch (error) {
+    console.error("Error while updating parking spots:", error.message);
+  }
+});
--- services/data-retention-service.js
+++ services/data-retention-service.js
@@ -2,6 +2,22 @@ class DataRetentionService {
   constructor(db) {
     this.db = db;
   }
+
+  async cleanUpParkingHistory() {
+    const query = `
+      DELETE FROM public."parking_spot_histories"
+      WHERE updated_at < NOW() - INTERVAL '3 days';
+    `;
+
+    try {
+      const result = await this.db.query(query);
+      return result.rowCount > 0;
+    } catch (error) {
+      throw new Error(
+        `Unable to clean up history of parking spots: ${error.message}`
+      );
+    }
+  }
 }
 
 module.exports = DataRetentionService;
